4498844691d8268521ad8c0053ed08803fe79497
Brady <bradychen410@gmail.com>
2023-05-27 16:16:11 +0800

    Turn to iMac

--- ZJ/test.cpp
+++ ZJ/test.cpp
@@ -1,55 +1,10 @@
-#include <algorithm>
-#include <iostream>
-
+#include <bits/stdc++.h>
+#define ouo ios_base::sync_with_stdio(false), cin.tie(0)
+#define ll long long
+#define db double
 using namespace std;
 
-class student {
-   public:
-    int id;
-
-    string no;
-
-    string name;
-};
-
-bool cmp(student i, student j) {
-    if (i.no[8] == j.no[8]) {
-        if (i.no[0] == j.no[0]) {
-            return i.id < j.id;
-
-        }
-
-        else {
-            return i.no[0] < j.no[0];
-        }
-
-    }
-
-    else {
-        return i.no[8] < j.no[8];
-    }
-}
-
 int main() {
-    ios::sync_with_stdio(0);
-
-    cin.tie(0);
-
-    int n;
-
-    string s;
-
-    student a[100];
-
-    cin >> n;
-
-    for (int i = 0; i < n; i++) {
-        cin >> a[i].no >> a[i].name, a[i].id = i;
-    }
-
-    sort(a, a + n, cmp);
-
-    for (int i = 0; i < n; i++) {
-        cout << a[i].no[8] << ": " << a[i].name << endl;
-    }
+    for (int i = 0; i < 10; ++i) cout << i;
+    return 0;
 }
