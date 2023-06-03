fa5ec948a095b41756f2b5aaa32d0afbc52ecbd1
Brady <bradychen410@gmail.com>
2023-06-03 16:13:36 +0800

    update

--- ZJ/done/i402.cpp
+++ ZJ/done/i402.cpp
@@ -1,49 +1,60 @@
-#include <iostream>
-#include <algorithm>
-#include <cstring>
+#include <bits/stdc++.h>
+#define ouo ios_base::sync_with_stdio(false), cin.tie(0)
+#define ll long long
+#define db double
 using namespace std;
 
+int n, m;
+int a[1001], b[1001], r[1001];
+int ans = INT_MIN;
+
+void f(int x, int y) {
+    int sum = 0;
+    while (x < n && y < m) {
+        sum += a[x] * b[y];
+        ans = max(sum, ans);
+        if (sum < 0) sum = 0;
+        x++;
+        y++;
+    }
+    // ans = max(sum, ans);
+}
+
+void r_f(int x, int y) {
+    int sum = 0;
+    while (x < n && y < m) {
+        sum += a[x] * r[y];
+        ans = max(sum, ans);
+        if (sum < 0) sum = 0;
+        x++;
+        y++;
+    }
+    // ans = max(sum, ans);
+}
+
 int main() {
-    int n, m;
     cin >> n >> m;
-    int grid[n][m];
-    int nn[n], mm[m];
-    for (int s = 0; s < n; s++)
-        cin >> nn[s];
-    for (int s = m-1; s >= 0; s--)
-        cin >> mm[s];
-    memset(grid, 0, sizeof(grid));
-    for (int i = 0; i < n; i++)
-        grid[i][0] = mm[0] * nn[i];
-    for (int i = 0; i < m; i++)
-        grid[0][i] = mm[i] * nn[0];
-    //
-    int max = grid[0][0];
-    for (int j = 1; j < m; j++) {
-        for (int i = 1; i < n; i++) {
-            grid[i][j] = nn[i] * mm[j];
-            if (grid[i-1][j-1] > 0)
-                grid[i][j] += grid[i-1][j-1];
-            if (grid[i][j] > max)
-                max = grid[i][j];
-        }
+    for (int s = 0; s < n; s++) {
+        cin >> a[s];
     }
-    //
-    reverse(mm, mm + m);
-    memset(grid, 0, sizeof(grid));
-    for (int i = 0; i < n; i++)
-        grid[i][0] = mm[0] * nn[i];
-    for (int i = 0; i < m; i++)
-        grid[0][i] = mm[i] * nn[0];
-    //
-    for (int j = 1; j < m; j++) {
-        for (int i = 1; i < n; i++) {
-            grid[i][j] = nn[i] * mm[j];
-            if (grid[i-1][j-1] > 0)
-                grid[i][j] += grid[i-1][j-1];
-            if (grid[i][j] > max)
-                max = grid[i][j];
-        }
+    for (int s = 0; s < m; s++) {
+        cin >> b[s];
+        r[m-s-1] = b[s];
     }
-    cout << max;
+    for (int i = 0; i < n; i++) {
+        f(i, 0);
+        r_f(i, 0);
+    }
+    for (int i = 0; i < n; i++) {
+        f(0, i);
+        r_f(0, i);
+    }
+    // for (int i = 0; i < n; i++) {
+    //     for (int j = 0; j < m; j++) {
+    //         f(i, j);
+    //         r_f(i, j);
+    //     }
+    // }
+    cout << ans << endl;
+    return 0;
 }
