42a0536279642c9bc9de629dbe54f4c887981536
Brady <bradychen410@gmail.com>
2023-06-04 08:34:07 +0800

    update

--- ZJ/test.cpp
+++ ZJ/test.cpp
@@ -1,12 +1,13 @@
-#include <iostream>
-#include <iomanip>
+#include <bits/stdc++.h>
 #define ouo ios_base::sync_with_stdio(false), cin.tie(0)
 #define ll long long
 #define db double
 using namespace std;
 
 int main() {
-    db n = 3.14159;
-    cout << fixed << setprecision(3) << n << endl;
+    map<string, string> mp;
+    mp["r123"] = "testing";
+    // auto iter = mp.find("r123");
+    cout << mp["r123"];
     return 0;
 }
