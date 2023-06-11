296b6670d13c958c7acca7a100c11ad58de6fac2
Brady <bradychen410@gmail.com>
2023-06-11 10:31:49 +0800

    update

---
+++ CSF/final/f1.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define ouo ios_base::sync_with_stdio(false), cin.tie(0)
+#define ll long long
+#define db double
+using namespace std;
+
+int main() {
+    ouo;
+    return 0;
+}
