1063d6e20b3a5b00161fac92c159f4d1175ab614
Brady <bradychen410@gmail.com>
2024-01-25 11:13:45 +0800

    0125

--- PY/0123-1.py
+++ PY/0123-1.py
@@ -1,9 +1,7 @@
-def gcd(a, b):
-    for i in range(min(a, b), 0, -1):
-        if a % i == 0 and b % i == 0:
-            return i
+a, b = input().split()
+a, b = int(a), int(b)
+for i in range(min(a, b), 1, -1):
+    if a % i == 0 and b % i == 0:
+        print(i)
+        exit()
 
-a = 12
-b = 18
-result = gcd(a, b)
-print("The greatest common divisor of", a, "and", b, "is", result)
